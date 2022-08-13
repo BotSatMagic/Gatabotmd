ff65ba2ce5794704d0889dccebb23ff1dad2970f
BotSatMagic <victor-albert2022@outlook.com>
2022-08-13 14:21:12 -0500

    yo

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -1,8 +1,11 @@
 let handler = m => m
 handler.all = async function (m) {
 let chat = global.db.data.chats[m.chat]
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
 
 if (/^hola$/i.test(m.text) && chat.audios && !chat.isBanned) {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
 let vn = './media/Hola.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
