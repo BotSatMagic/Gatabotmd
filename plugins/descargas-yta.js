89da59c5d3b92724a96a3bcb3fba6b1634c6092b
BotSatMagic <victor-albert2022@outlook.com>
2022-08-19 16:32:53 -0500

    yo

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -7,7 +7,7 @@ try {
 let chat = global.db.data.chats[m.chat]
 const isY = /y(es)/gi.test(args[1])
 const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
-const limitedSize = (isPrems || isOwner ? 350 : 200) * 3074
+const limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
 let audio, source, res, link, lastError, isLimit
 for (let i in _audio) {
 try {
